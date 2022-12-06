e0aeb586de180d15e43bb75e2dc2d762052d8346
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2022-12-06 14:57:13 +0600

    reverse text

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	util "golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	message := "Hello, OTUS!"
+	fmt.Print(util.Reverse(message))
 }
